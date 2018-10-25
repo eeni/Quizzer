8e8daef8f174591467f44d618ba2c600d16e42e9
eni <eenifezollari@gmail.com>
2018-10-25 09:56:50 +0200

    Dont require quizz options for the moment

--- server/validators/quiz-validator.js
+++ server/validators/quiz-validator.js
@@ -3,10 +3,10 @@ const Validator = require('../base/validator')
 class QuizCreateValidator extends Validator {
   data(joi) {
     return {
-      quiz_name: joi.string().min(3).required().required(),
-      topics: joi.array().strict().items(joi.string()).required(),
-      subtopics: joi.array().strict().items(joi.string()).required(),
-      difficulty: joi.number().min(1).max(5).required(),
+      quiz_name: joi.string().min(3),
+      topics: joi.array().strict().items(joi.string()),
+      subtopics: joi.array().strict().items(joi.string()),
+      difficulty: joi.number().min(1).max(5),
     }
   }
 }
